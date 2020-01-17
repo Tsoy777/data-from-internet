680b89825b65f27d560b4859a46a7554426c28d9
Tsoy777 <54719369+Tsoy777@users.noreply.github.com>
2020-01-18 00:11:21 +0300

    Update pipelines.py

--- avitoparse/pipelines.py
+++ avitoparse/pipelines.py
@@ -5,7 +5,13 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
+from pymongo import MongoClient
+
+mongo_client = MongoClient()
 
 class AvitoparsePipeline(object):
     def process_item(self, item, spider):
+        database = mongo_client[spider.name]
+        collection = database['avito_parse']
+        collection.insert_one(item)
         return item
